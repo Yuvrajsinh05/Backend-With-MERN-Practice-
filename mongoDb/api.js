7c0f3d52190528876133023a06714bb69aa19db2
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-11-13 11:29:26 +0530

    post

--- mongoDb/api.js
+++ mongoDb/api.js
@@ -1,6 +1,9 @@
+const { json } = require('express');
 const express =require('express')
 const dbConnect = require('./config')
 const app= express();
+
+app.use(express.json())
 
 app.get('/',async(req,res)=>{
     let data = await dbConnect
@@ -8,7 +11,10 @@ app.get('/',async(req,res)=>{
     console.log(data)
     res.send(data)
 })
-app.post('/',(req,res)=>{
-    
+app.post('/',async(req,res)=>{
+    console.log(req.body)    
+    let data = await dbConnect
+    let result = data.insertMany(req.body)
+    res.send(result)
 })
 app.listen(5000)
